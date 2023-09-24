2cca309be6f6faf11c539381f500ec4691eff852
ChapaStone1 <chapapr@gmail.com>
2023-09-24 12:46:18 -0300

    TP2a finalizado

--- bigsix-con-hilos.c
+++ bigsix-con-hilos.c
@@ -4,30 +4,40 @@
 #define NUMBER_OF_THREADS 2
 #define CANTIDAD_INICIAL_HAMBURGUESAS 20
 int cantidad_restante_hamburguesas = CANTIDAD_INICIAL_HAMBURGUESAS; 
+int turno = 0;
 
+void *comer_hamburguesa(void *tid){
+	int thread_id = *((int *)tid);
+	while (1==1){ //repetir infinitamente hasta que fuerce la terminacion del hilo con pthread_exit
 
-void *comer_hamburguesa(void *tid) { 
-while(1 == 1){ //repetir infinitamente hasta que fuerce la terminacion del hilo con pthread_exit
-	
-	if (cantidad_restante_hamburguesas > 0) {
-	printf("Hola! soy el hilo(comensal) %d , me voy a comer una hamburguesa ! ya que todavia queda/n %d \n",tid, cantidad_restante_hamburguesas);
-	cantidad_restante_hamburguesas--; //me como una hamburguesa
-	} else
-	{
-	printf("SE TERMINARON LAS HAMBURGUESAS :( \n"); 
-	pthread_exit(NULL); //forzar terminacion del hilo
+		while (cantidad_restante_hamburguesas>0){
+			while (thread_id != turno){}
+				if (cantidad_restante_hamburguesas > 0) {
+					printf("Hola! soy el hilo(comensal) %d , me voy a comer una hamburguesa ! ya que todavia queda/n %d \n",thread_id, cantidad_restante_hamburguesas);
+					cantidad_restante_hamburguesas--; //me como una hamburguesa
+				if (turno!=0){
+					turno = 0;
+				}
+				else{
+					turno = 1;
+				}
+				}
+		}
+		printf("SE TERMINARON LAS HAMBURGUESAS :( \n"); 
+		pthread_exit(NULL); //forzar terminacion del hilo
 	}
 
 }
 
-}
 
 int main(int argc, char *argv[]) { 
 	pthread_t threads[NUMBER_OF_THREADS]; 
 	int status,i,ret ; 
+	int thread_ids[NUMBER_OF_THREADS];
 	for(int i=0; i < NUMBER_OF_THREADS; i++) { 
+		thread_ids[i] = i;
 		printf("Hola!, soy el hilo principal. Estoy creando el hilo %d \n", i); 
-		status = pthread_create(&threads[i], NULL, comer_hamburguesa, (void *)i); 
+		status = pthread_create(&threads[i], NULL, comer_hamburguesa, (void *)&thread_ids[i]); 
 		if (status != 0) { 
 		printf("Algo salio mal, al crear el hilo recibi el codigo de error %d \n", status);
 		exit(-1); 
